1fcbb66047a970e41a8340f02e44663d8a6f5856
gilpasternak35 <gilpasternak35@gmail.com>
2024-03-11 11:19:07 -0700

    improved crop

--- train.py
+++ train.py
@@ -2,11 +2,15 @@ from torch.utils.data import DataLoader
 from load_data import FacesDataset
 import matplotlib.pyplot as plt
 
-path_to_data = "C:/Users/maxes/Downloads/part1"
+# initializing dataloader upon pytorch dataset
+path_to_data = "part1"
 dset = FacesDataset(path_to_data)
 loader = DataLoader(dset, batch_size=4)
 
+# printing out by batch
 for batch in loader:
     print(len(batch))
+
+    # showing image, re-permuting so that pixel channels appear first
     plt.imshow(batch[0].permute(1, 2, 0))
     plt.show()
