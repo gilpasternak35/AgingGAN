07a9736a96d2461dadfd006fcf3e41082a3a44e8
Max Shepherd <mashepherd@ucsd.edu>
2024-03-13 18:50:45 -0700

    Finished initial cyclegan with readme

--- split_data.py
+++ split_data.py
@@ -1,16 +1,21 @@
 import os
 import shutil
 
-path = "C:/Users/maxes/Downloads/part1"
+path_to_data = "utkcropped"
 
-img_paths = [os.path.join(path, fname) for fname in os.listdir(path)]
+img_paths = [os.path.join(path_to_data, fname) for fname in os.listdir(path_to_data)]
+
+out_dir = "pytorch-CycleGAN-and-pix2pix/datasets/young2old_cropped"
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+    os.mkdir(out_dir + "/trainA")
+    os.mkdir(out_dir + "/trainB")
 
 for img_path in img_paths:
     img_name = os.path.basename(img_path).split('/')[-1]
     age = int(img_name.split('_')[0])
-    if 20 <= age <= 40:
-        shutil.copy(img_path, "trainA/" + img_name)
-    
-    if 60 <= age <= 100:
-        shutil.copy(img_path, "trainB/" + img_name)
-    
+    if 18 <= age <= 25:
+        shutil.copy(img_path, out_dir + "/trainA/" + img_name)
+
+    if 70 <= age:
+        shutil.copy(img_path, out_dir + "/trainB/" + img_name)
